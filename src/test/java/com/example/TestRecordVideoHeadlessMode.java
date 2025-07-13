04822bdac4e6c5b5a9e290dc3fc15a5d15a87fec
cuxuanthoai <cuxuanthoai@email.com>
2025-07-13 10:22:27 +0700

    add macos

--- src/test/java/com/example/TestRecordVideoHeadlessMode.java
+++ src/test/java/com/example/TestRecordVideoHeadlessMode.java
@@ -17,7 +17,7 @@ public class TestRecordVideoHeadlessMode {
 
     // Should close current chrome to improve performance capture
     @Test()
-    public void testRecordVideoHeadlessModeWindows() throws Exception {
+    public void testRecordVideoHeadlessModeWindow() throws Exception {
 
         // // Step 1: Start background screencast (non-blocking, silent)
         Thread screencastThread = new Thread(() -> {
@@ -56,7 +56,7 @@ public class TestRecordVideoHeadlessMode {
 
     }
 
-    @Test(enabled = false)
+    @Test()
     public void testRecordVideoHeadlessModeLinux() throws Exception {
 
         // Start background screencast (non-blocking, silent)
@@ -64,6 +64,48 @@ public class TestRecordVideoHeadlessMode {
             executeCMDInSilentMode("chmod +x ./screencastlinux");
 
             executeCMDInSilentMode("./screencastlinux -folder ./images");
+
+        });
+
+        screencastThread.setDaemon(true);
+        screencastThread.start();
+
+        // Setup Selenium in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-debugging-port=9222");
+        // options.addArguments("--user-data-dir=C:\\chrome-data");
+        options.addArguments("--mute-audio");
+        options.addArguments("--headless=new");
+
+        // Launch browser
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.youtube.com/watch?v=JVlDTK2-hUI");
+
+        Thread.sleep(3_000);
+        // force play video
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.SPACE).perform(); // Toggle play/pause
+
+        Thread.sleep(20_000); // Watch video for 20 seconds
+        driver.quit();
+
+        // Hard code to convert
+        // Hardcoded FFmpeg command for Linux
+        String cmdConvert = "ffmpeg -y -framerate 25 -i images/screenshot_%06d.png "
+                + "-vf scale=trunc(iw/2)*2:trunc(ih/2)*2 -c:v libx264 -pix_fmt yuv420p output.mp4";
+
+        executeCMDInSilentMode(cmdConvert);
+
+    }
+
+    @Test()
+    public void testRecordVideoHeadlessModeMac() throws Exception {
+
+        // Start background screencast (non-blocking, silent)
+        Thread screencastThread = new Thread(() -> {
+            executeCMDInSilentMode("chmod +x ./screencastMacIntel");
+
+            executeCMDInSilentMode("./screencastMacIntel -folder ./images");
 
         });
 
